67eaa03b9643c29cc72f647c00c168d5a371070c
vhlinh <vuhoanglinh1997@gmail.com>
2018-05-10 16:53:08 +0200

    Hooked input: "left arrow" / Finished right<->left transition

--- Test/scripts/Bezier.gd
+++ Test/scripts/Bezier.gd
@@ -1,18 +1,21 @@
 extends Node2D
 
-onready var P1 = get_node("P1")
-onready var P2 = get_node("P2")
-onready var P3 = get_node("P3")
-onready var P4 = get_node("P4")
-onready var FollowP1 = get_node("Path P1/PathFollow2D")
-onready var FollowP2 = get_node("Path P2/PathFollow2D")
-onready var FollowP3 = get_node("Path P3/PathFollow2D")
-onready var Tween = get_node("Tween")
+onready var P1 = get_node("Points/P1")
+onready var P2 = get_node("Points/P2")
+onready var P3 = get_node("Points/P3")
+onready var P4 = get_node("Points/P4")
+onready var Paths = get_node("Paths")
+onready var PathP1 = get_node("Paths/PathP1/PathFollow2D")
+onready var PathP2 = get_node("Paths/PathP2/PathFollow2D")
+onready var PathP3 = get_node("Paths/PathP3/PathFollow2D")
+onready var BendTween = get_node("BendTween")
+onready var RecoverTween = get_node("RecoverTween")
 
-export var SEGMENT_COUNT = 50
-export var BEND_DURATION = 3.0
-export var RECOVER_DURATION = 1.5
+export (int) var SEGMENT_COUNT = 50
+export (float) var BEND_DURATION = 3.0
+export (float) var RECOVER_DURATION = 1.5
 
+var direction = 0
 var bezier_curve = PoolVector2Array()
 
 func _ready():
@@ -23,12 +26,22 @@ func _process(delta):
 	calculate_curve()
 	update()
 	
-	if Input.is_action_just_pressed("ui_right"):
-		bend([FollowP1, FollowP2, FollowP3], 
-				BEND_DURATION, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
+	if direction != 0:
+		Paths.scale.x = direction
+	
+	if Input.is_action_pressed("ui_right"):
+		assign_direction(1)
+		if direction == 1 and not BendTween.is_active():
+			bend([PathP1, PathP2, PathP3], BEND_DURATION)
 	elif Input.is_action_just_released("ui_right"):
-		recover([FollowP1, FollowP2, FollowP3],
-				RECOVER_DURATION, Tween.TRANS_QUINT, Tween.EASE_OUT)
+		recover([PathP1, PathP2, PathP3], RECOVER_DURATION)
+	
+	if Input.is_action_pressed("ui_left"):
+		assign_direction(-1)
+		if direction == -1 and not BendTween.is_active():
+			bend([PathP1, PathP2, PathP3], BEND_DURATION)
+	elif Input.is_action_just_released("ui_left"):
+		recover([PathP1, PathP2, PathP3], RECOVER_DURATION)
 	pass
 
 func _draw():
@@ -50,24 +63,29 @@ func _draw():
 		draw_line(bezier_curve[i], bezier_curve[i+1], Color(1.0, 1.0, 1.0), 1.5, true)
 	pass
 
-func bend(paths, duration, trans_type, ease_type):
-	Tween.remove_all()
-	for i in range(paths.size()):
-		var current_offset = paths[i].unit_offset
-		Tween.interpolate_property(paths[i], "unit_offset", current_offset, 1,
-						(1-current_offset) * duration, trans_type, ease_type)
-	
-	Tween.start()
+func assign_direction(dir):
+	if self.direction == 0:
+		direction = dir
 	pass
 
-func recover(paths, duration, trans_type, ease_type):
-	Tween.remove_all()
+func bend(paths, duration):
+	RecoverTween.remove_all()
 	for i in range(paths.size()):
 		var current_offset = paths[i].unit_offset
-		Tween.interpolate_property(paths[i], "unit_offset", current_offset, 0,
-						current_offset * duration, trans_type, ease_type)
+		BendTween.interpolate_property(paths[i], "unit_offset", current_offset, 1,
+						(1-current_offset) * duration, Tween.TRANS_CUBIC, Tween.EASE_OUT)
 	
-	Tween.start()
+	BendTween.start()
+	pass
+
+func recover(paths, duration):
+	BendTween.remove_all()
+	for i in range(paths.size()):
+		var current_offset = paths[i].unit_offset
+		RecoverTween.interpolate_property(paths[i], "unit_offset", current_offset, 0,
+						current_offset * duration, Tween.TRANS_CUBIC, Tween.EASE_IN)
+	
+	RecoverTween.start()
 	pass
 
 func calculate_curve():
@@ -84,3 +102,8 @@ func calculate_point(t, p1, p2, p3, p4):
 	var ttt = tt * t
 	
 	return uuu*p1 + 3*uu*t*p2 + 3*u*tt*p3 + ttt*p4
+
+func _on_RecoverTween_tween_completed(object, key):
+	if direction != 0:
+		direction = 0
+	pass # replace with function body
